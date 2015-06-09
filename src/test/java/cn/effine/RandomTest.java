27cfb0b3a52eb6c617fcc880b170c5337500daff
verphen <verphen@163.com>
2015-06-10 00:26:40 +0800

    update test

--- src/test/java/cn/effine/RandomTest.java
+++ src/test/java/cn/effine/RandomTest.java
@@ -2,12 +2,11 @@ package cn.effine;
 
 public class RandomTest {
 
-	private static final int i=9;
-	
+	private static final int i = 9;
+
 	public static void main(String[] args) {
-		
-		i = 10;
+
 		System.out.println();
-		
+
 	}
 }
