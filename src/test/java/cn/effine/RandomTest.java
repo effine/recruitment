7171d8795e06d09935f311915506f4c23294fef6
effine <verphen@163.com>
2015-06-24 23:11:26 +0800

    update

--- src/test/java/cn/effine/RandomTest.java
+++ src/test/java/cn/effine/RandomTest.java
@@ -2,11 +2,17 @@ package cn.effine;
 
 public class RandomTest {
 
-	private static final int i = 9;
-
 	public static void main(String[] args) {
 
-		System.out.println();
+		String s1 = "helloword";
+		String s2 = new String("helloword");
+		
+		System.out.println(s1==s2);
+		
+		Integer i1 = 2;
+		Integer i2 = 2;
+		System.err.println(i1==i2);
+		
 
 	}
 }
